4f773674b89148f980235eeeeb4ff7c089fec988
Meng Ronal <mengronal@gmail.com>
2025-03-28 09:37:37 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include './include/header.php';
+?>
+</head>
+<body>
+    <div class="container">
+        <div class="content_left">
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, eius.</p>
+            </div>
+        </div>
+        <div class="content_right">
+            <div class="content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, ipsam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem at quae neque, explicabo facere blanditiis ducimus magni? Laborum voluptate quaerat veniam labore quas quia voluptatum accusantium adipisci, ipsum, fugit quisquam.
+            </div>
+        </div>
+    </div>
+</body>
+</html>
